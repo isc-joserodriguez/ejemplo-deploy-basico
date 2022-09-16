a8e036c9af73178b3e73fa596d6371d1d2a17359
Jose Rodriguez <isc.joserodriguez@gmail.com>
2022-09-15 18:38:24 -0600

    Version 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Versión 1</h1>
+        <h1>Versión 2</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
